b237bde93ff0d6ed565fae16858b1844c2deb86e
Joshua Martinez <Joshuamartinez8723@gmail.com>
2020-06-05 13:58:09 -0600

    Clases

---
+++ src/lab5_joshuamartinez/Escuadron.java
@@ -0,0 +1,66 @@
+package lab5_joshuamartinez;
+
+import java.util.ArrayList;
+
+public class Escuadron {
+    private String nombre, lugar_base, lider, tipo;
+    private ArrayList lista = new ArrayList();
+
+    public Escuadron() {
+    }
+
+    public Escuadron(String nombre, String lugar_base, String lider, String tipo) {
+        this.nombre = nombre;
+        this.lugar_base = lugar_base;
+        this.lider = lider;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLugar_base() {
+        return lugar_base;
+    }
+
+    public void setLugar_base(String lugar_base) {
+        this.lugar_base = lugar_base;
+    }
+
+    public String getLider() {
+        return lider;
+    }
+
+    public void setLider(String lider) {
+        this.lider = lider;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public ArrayList getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList lista) {
+        this.lista = lista;
+    }
+
+    @Override
+    public String toString() {
+        return "Escuadron{" + "nombre=" + nombre + ", lugar_base=" + lugar_base + ", lider=" + lider + ", tipo=" + tipo + ", lista=" + lista + '}';
+    }
+    
+    
+    
+}
